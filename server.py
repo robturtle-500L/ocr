d0b9e10316ca8da1c0ac20865343cb73e72ba91a
Yang Liu <jeremyrobturtle@gmail.com>
2020-08-16 18:31:19 -0500

    save samples to CSV files

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 HIDDEN_NODE_COUNT = 15
+TRAINING_SET_FILE_NAME = 'training_set.csv'
+TEST_SET_FILE_NAME = 'test_set.csv'
 
 app = Flask(__name__)
 CORS(app)
@@ -21,8 +23,21 @@ def about():
 @app.route('/', methods=['POST'])
 def add_sample():
     sample = request.json
+    sample_type = sample['type']
+    record = "{},{}\n".format(sample['label'], sample['y0'])
+    if sample_type == 'train':
+        with open(TRAINING_SET_FILE_NAME, 'a') as f:
+            f.write(record)
+        return jsonify(success=True)
+    elif sample_type == 'test':
+        with open(TEST_SET_FILE_NAME, 'a') as f:
+            f.write(record)
+        return jsonify(type='test', result=6)
+    else:
+        return jsonify(error='Unsupported type: {}'.format(sample_type))
     return jsonify(echo=sample)
 
 
-app.debug = True
-app.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
